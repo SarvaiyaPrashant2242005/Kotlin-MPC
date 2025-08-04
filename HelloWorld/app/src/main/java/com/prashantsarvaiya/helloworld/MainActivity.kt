8069b27af62a5a2c9f8d78f8ae381e6389dafd41
prashant sarvaiya <prashanthere90@gmail.com>
2025-08-04 09:25:47 +0530

    Basic Login - Exp1

---
+++ HelloWorld/app/src/main/java/com/prashantsarvaiya/helloworld/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.prashantsarvaiya.helloworld
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val editUsername = findViewById<EditText>(R.id.edt)
+        val editpwd = findViewById<EditText>(R.id.editPwd)
+        val btnLogin = findViewById<Button>(R.id.button)
+        btnLogin.setOnClickListener{
+            val username = editUsername.text.toString()
+            val pwd = editpwd.text.toString()
+            if(username == pwd){
+                Toast.makeText(this, "Welcome Mr."+username, Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Invalid Credentials", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+}
